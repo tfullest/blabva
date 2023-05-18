0fc0a180fed245406d7f3fa2fcabe9b763111cca
tfullest <orthotfulceo@gmail.com>
2023-05-18 17:59:20 -0400

    orthotics page js complete

--- modal.js
+++ modal.js
@@ -4,11 +4,14 @@
         extendViewableAFO: document.querySelector("[data-modal-open-afo]"),
        extendViewableCranial: document.querySelector("[data-modal-open-cranial"),
        extendViewableKafo: document.querySelector("[data-modal-open-kafo]"),
-
-
+        extendViewableSmo: document.querySelector("[data-modal-open-smo]"),
+extendViewableTlso: document.querySelector("[data-modal-open-tlso"),
        modalCranial: document.querySelector("[data-modal-cranial]"),
         modalAFO: document.querySelector("[data-modal-afo]"),
         modalKafo: document.querySelector("[data-modal-kafo"),
+        modalSmo: document.querySelector("[data-modal-smo"),
+        modalTlso: document.querySelector("[data-modal-tlso]"),
+
     
     };
 
@@ -28,6 +31,18 @@
     function toggleModalKafo(){
         refs.modalKafo.classList.toggle("is-hidden");
         refs.extendViewableKafo.classList.toggle("quetzalkafo");
+    }
+
+    refs.extendViewableSmo.addEventListener("click", toggleModalSmo);
+    function toggleModalSmo(){
+        refs.modalSmo.classList.toggle("is-hidden");
+        refs.extendViewableSmo.classList.toggle("quetzalsmo");
+    }
+
+    refs.extendViewableTlso.addEventListener("click", toggleModalTlso);
+    function toggleModalTlso(){
+        refs.modalTlso.classList.toggle("is-hidden");
+        refs.extendViewableTlso.classList.toggle("quetzaltlso");
     }
 }
 )();
