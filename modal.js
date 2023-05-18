29c5ac35565cbc69714dcf7646b0f6bec2a6266a
tfullest <orthotfulceo@gmail.com>
2023-05-18 17:50:21 -0400

    kafo js update

--- modal.js
+++ modal.js
@@ -1,17 +1,33 @@
 (()=>
 {
     const refs = {
-        extendViewable: document.querySelector("[data-modal-open]"),
-        retractViewable: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
+        extendViewableAFO: document.querySelector("[data-modal-open-afo]"),
+       extendViewableCranial: document.querySelector("[data-modal-open-cranial"),
+       extendViewableKafo: document.querySelector("[data-modal-open-kafo]"),
+
+
+       modalCranial: document.querySelector("[data-modal-cranial]"),
+        modalAFO: document.querySelector("[data-modal-afo]"),
+        modalKafo: document.querySelector("[data-modal-kafo"),
+    
     };
 
-    refs.extendViewable.addEventListener("click", toggleModal);
-    refs.retractViewable.addEventListener("click", toggleModal);
+    refs.extendViewableAFO.addEventListener("click", toggleModalAFO);
+    function toggleModalAFO(){
+        refs.modalAFO.classList.toggle("is-hidden");
+        refs.extendViewableAFO.classList.toggle("quetzalafo");
+    }
 
-    function toggleModal(){
-        refs.modal.classList.toggle("is-hidden");
-        refs.extendViewable.classList.toggle("quetzal");
+    refs.extendViewableCranial.addEventListener("click", toggleModalCranial);
+    function toggleModalCranial(){
+        refs.modalCranial.classList.toggle("is-hidden");
+        refs.extendViewableCranial.classList.toggle("quetzalCranial");
+    }
+
+    refs.extendViewableKafo.addEventListener("click", toggleModalKafo);
+    function toggleModalKafo(){
+        refs.modalKafo.classList.toggle("is-hidden");
+        refs.extendViewableKafo.classList.toggle("quetzalkafo");
     }
 }
 )();
